46179f253bfba95be38d0d24ff89c72ff2cc3a0c
nialegu <rastrast207@gmail.com>
2022-04-15 13:20:56 +0300

    'new_selected_sort_method_in_main_class'

--- src/main/java/org/sorting/Sorting.java
+++ src/main/java/org/sorting/Sorting.java
@@ -19,4 +19,21 @@ public class Sorting{
         }
         return mas;
     }
+
+    public int[] sortSelected(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int pos = i;
+            int min = array[i];
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
+                    pos = j;
+                    min = array[j];
+                }
+            }
+            array[pos] = array[i];
+            array[i] = min;
+        }
+        return array;
+    }
+
 }
